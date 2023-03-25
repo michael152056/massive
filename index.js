e3824609ce03b2cf85c73b2e095783a432fc89e8
Michael Robles <michaeljavier26@hotmail.com>
2023-03-25 16:17:44 -0500

    Configuración Railway

--- index.js
+++ index.js
@@ -4,19 +4,14 @@ const cors = require("cors");
 const app = express();
 const http = require('http').createServer(app);
 const bodyparser = require('body-parser');
-const serverless = require('serverless-http')
 app.use(cors());
 app.use(express.json({limit: '50mb'}));
 
-
-/* app.use('/api/messages', require('./routes/messages'));
- */
-app.use('/.netlify/functions/api/messages', require('../routes/messages'));
+app.use('/api/messages', require('./routes/messages'));
 
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({limit: '50mb', extended: false }));
 app.use(express.static('cliente'));
-
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'cliente/index.html'));
@@ -27,4 +22,4 @@ http.listen(process.env.PORT || 4000, () => {
 })
 
 
-module.exports.handler = serverless(app);
+module.exports = app;
